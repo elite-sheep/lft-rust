5bf07a4b00fae8e643d4b33fbadf18faeea3d23f
Bulbaberry <wyc8094@gmail.com>
2022-05-31 19:11:32 -0700

    Improved threadpool implementation
    - A improved threadpool implementation

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,47 @@ use crossbeam_channel::unbounded;
 mod naive_threadpool;
 use naive_threadpool::{ NaiveThreadPool };
 
+use std::{thread, time};
+
+mod threadpool;
+use threadpool::{ ThreadPool };
+
 fn main() {
     pretty_env_logger::init();
 
     let n_workers = 4;
     let n_jobs = 8;
-    let thread_pool = naive_threadpool::builder()
-        .num_workers(8)
+    let thread_pool = threadpool::builder()
+        .num_workers(1)
+        .max_thread_count(4)
         .thread_stack_size(8 * 1024 * 1024)
         .build();
     // thread_pool.start();
 
     let (tx, rx) = unbounded();
-    for i in 0..n_jobs+10 {
+    for i in 0..n_jobs+8 {
         trace!("hello, world: {} times.", i);
         let tx = tx.clone();
         thread_pool.execute(move || {
-            trace!("hello, world");
+            // trace!("hello, world: {}.", i);
+            let ten_millis = time::Duration::from_millis(128);
+            let now = time::Instant::now();
+
+            thread::sleep(ten_millis);
             tx.send(1).expect("channel will be there waiting for the pool");
         });
+        
+        if i < 4 {
+            thread_pool.spawn_extra_one_worker();
+        }
+
+        if i == 1 || i == 2 || i == 5 || i == 6 {
+            thread_pool.shutdown_one_worker();
+        }
     }
     thread_pool.join();
 
-    rx.iter().take(n_jobs+10).fold(0, |i, j| {
+    rx.iter().take(n_jobs+8).fold(0, |i, j| {
         trace!("Hello, world: {}, {}.", i, j);
         i+j
     });
